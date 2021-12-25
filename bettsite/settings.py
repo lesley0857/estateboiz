7165d8d701813b3bca463063f4f49eb949b6c8a2
lesley <nwekelesley@gmail.com>
2021-12-25 18:06:26 +0100

    'intial'

--- bettsite/settings.py
+++ bettsite/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     #own
     'England',
     'rest_framework',
-    'rest_framework_simplejwt',
-    #'rest_framework_jwt'
+    #'rest_framework_simplejwt',
+    'rest_framework_jwt',
 
 ]
 
