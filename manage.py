d59d787d857d35e708f3d43ba8bb346c85bd4dd2
lesley <nwekelesley@gmail.com>
2021-12-29 10:47:29 +0100

    'op'

--- manage.py
+++ manage.py
@@ -8,16 +8,12 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bettsite.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError :
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
