373b3ffd03dedd5b9e4c11a05596a63d3e284b43
lesley <nwekelesley@gmail.com>
2021-12-31 02:27:37 +0100

    'yuo'

--- bettsite/wsgi.py
+++ bettsite/wsgi.py
@@ -29,7 +29,7 @@ app = socketio.WSGIApp(sio,application)
 ON_HEROKU = os.environ.get('ON_HEROKU')
 if ON_HEROKU:
     # get the heroku port
-    port = int(os.environ.get("PORT", 17995))  # as per OP comments default is 17995
+    port = int(os.environ.get("PORT", 8000))  # as per OP comments default is 17995
 else:
     port = 8000
 
